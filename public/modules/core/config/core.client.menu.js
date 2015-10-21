50552869ac7c7135a391ee4f41086cc33cab23be
isbiin <isalazar@biinapp.com>
2015-10-21 12:08:41 -0600

    adding old showcase assignment chnaging sidebar object colors removing billing section from profile adding administration dropdown

--- public/modules/core/config/core.client.menu.js
+++ public/modules/core/config/core.client.menu.js
@@ -14,7 +14,16 @@
         Menus.addMenuItem('sidebar', 'Elements', 'elements', null, '/elements', false, null, null,'icon-book-open',null);
         Menus.addMenuItem('sidebar', 'Showcase', 'showcases', null, '/showcase', false, null, null,'icon-docs',null);
         Menus.addMenuItem('sidebar', 'Biins', 'biins', null, '/biins', false, null, null,'icon-feed',null);
-        Menus.addMenuItem('sidebar', 'Profile', 'profile', null, '/profile', false, null, null,'icon-user',null);
+
+        //Menus.addSubMenuItem('sidebar', 'dashboard', 'Dashboard', 'dashboard');
+        //Menus.addSubMenuItem('sidebar', 'dashboard', 'Dashboard v2', 'dashboard/v2');
+        //Menus.addSubMenuItem('sidebar', 'dashboard', 'Dashboard v3', 'dashboard/v3');
+
+        //Menus.addMenuItem('sidebar', 'Dashboard', 'dashboard', 'dropdown', null, true, null, 1, 'icon-speedometer');
+        Menus.addMenuItem('sidebar', 'Administration', 'profile', 'dropdown', null, false, null, null,'icon-feed');
+        //this.addSubMenu   (menuId, rootMenuItemURL, menuItemTitle, menuItemURL, menuItemUIRoute, isPublic, roles, position) {
+        Menus.addSubMenuItem('sidebar', 'profile', 'Profile','profile',null, false, null, null);
+        Menus.addSubMenuItem('sidebar', 'profile', 'Organizaciones','organzations',null, false, null, null);
         Menus.addMenuItem('sidebar', 'Maintenance', 'maintenance', null, '/maintenance', false, null, null,'icon-settings',null);
     }
 
