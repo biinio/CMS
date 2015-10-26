4fd83e1dea37a4a1d8e94e1f17a262a9fb320354
isbiin <isalazar@biin.io>
2015-10-26 09:25:23 -0600

    adding chalk update and fixed crash in config

--- public/config.js
+++ public/config.js
@@ -5,7 +5,15 @@ var ApplicationConfiguration = (function() {
 	// Init module configuration options
 	var applicationModuleName = 'BiinCMSApp';
 
-	var applicationBackendURL =  process.env.BACK_END_URL || 'https://qa-biinapp.herokuapp.com/';
+	var applicationBackendURL = window.location.href.indexOf('angle-biin') > -1 ? 'https://qa-biinapp.herokuapp.com/' :
+		window.location.href.indexOf('dev') > -1 ? 'https://dev-biinapp.herokuapp.com/' :
+			window.location.href.indexOf('qa') > -1 ?'https://qa-biinapp.herokuapp.com/' :
+				window.location.href.indexOf('demo') > -1 ? 'https://demo-biinapp.herokuapp.com/' :
+					window.location.href.indexOf('production') > -1 ? 'https://biin.io/' :
+						window.location.href.indexOf('biin.io') > -1 ? 'https://biin.io/' :
+							window.location.href.indexOf('localhost') > -1 ? 'https://dev-biinapp.herokuapp.com/' : '';
+
+
 
 	var applicationModuleVendorDependencies = ['ngRoute', 'ngAnimate', 'ngStorage', 'ngTouch', 'ngCookies',
         'pascalprecht.translate', 'ui.bootstrap', 'ui.router', 'oc.lazyLoad', 'cfp.loadingBar', 'ngSanitize',
