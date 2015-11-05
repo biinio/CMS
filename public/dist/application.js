fedfadcc64fb6ecf7a2e401be6d7fd0ff216d974
isbiin <isalazar@biin.io>
2015-11-05 09:18:09 -0600

    adding new changes to master

--- public/dist/application.js
+++ public/dist/application.js
@@ -974,16 +974,17 @@ angular.module('biins').config(['$stateProvider',
     function coreMenu(Menus) {
         // Add default menu entry
         //Menus.addMenuItem('sidebar', 'Home', 'home', null, '/home', true, null, null, 'icon-home');
-        Menus.addMenuItem('sidebar', 'Dashboard', 'dashboard', null, '/dashboard', false, null, null,'icon-speedometer',null);
-        Menus.addMenuItem('sidebar', 'Sites', 'sites', null, '/sites', false, null, null,'icon-pointer',null);
-        Menus.addMenuItem('sidebar', 'Elements', 'elements', null, '/elements', false, null, null,'icon-book-open',null);
-        Menus.addMenuItem('sidebar', 'Showcase', 'showcases', null, '/showcase', false, null, null,'icon-docs',null);
-        Menus.addMenuItem('sidebar', 'Biins', 'biins', null, '/biins', false, null, null,'icon-feed',null);
-        Menus.addMenuItem('sidebar', 'Administration', 'profile', 'dropdown', null, false, null, null,'fa fa-gears');
+
+        Menus.addMenuItem('sidebar', 'Dashboard', 'dashboard', null, '/dashboard', false, null, null,'icon-speedometer', "sidebar.MENU_DASHBOARD");
+        Menus.addMenuItem('sidebar', 'Sites', 'sites', null, '/sites', false, null, null,'icon-pointer', "sidebar.MENU_SITES");
+        Menus.addMenuItem('sidebar', 'Elements', 'elements', null, '/elements', false, null, null,'icon-book-open', "sidebar.MENU_ELEMENTS");
+        Menus.addMenuItem('sidebar', 'Showcase', 'showcases', null, '/showcase', false, null, null,'icon-docs', "sidebar.MENU_SHOWCASES");
+        Menus.addMenuItem('sidebar', 'Biins', 'biins', null, '/biins', false, null, null,'icon-feed', "sidebar.MENU_BIINS");
+        Menus.addMenuItem('sidebar', 'Administration', 'administration', 'dropdown', null, false, null, null,'fa fa-gears', "sidebar.MENU_ADMINISTRATION");
         //this.addSubMenu   (menuId, rootMenuItemURL, menuItemTitle, menuItemURL, menuItemUIRoute, isPublic, roles, position) {
-        Menus.addSubMenuItem('sidebar', 'profile', 'Profile','profile',null, false, null, null);
-        Menus.addSubMenuItem('sidebar', 'profile', 'Organizaciones','organization',null, false, null, null);
-        Menus.addMenuItem('sidebar', 'Maintenance', 'maintenance', null, '/maintenance', false, null, null,'icon-settings',null);
+        Menus.addSubMenuItem('sidebar', 'administration', 'Profile','profile',null, false, null, null, "sidebar.MENU_PROFILE");
+        Menus.addSubMenuItem('sidebar', 'administration', 'Organizaciones','organization',null, false, null, null, "sidebar.MENU_ORGANIZATIONS");
+        Menus.addMenuItem('sidebar', 'Maintenance', 'maintenance', null, '/maintenance', false, null, null,'icon-settings',"sidebar.MENU_MAINTENANCE");
     }
 
 })();
@@ -1256,6 +1257,7 @@ angular.module('app.core').service('Menus', [
 
 			// Push new menu item
 			this.menus[menuId].items.push({
+				//title: menuItemTitle,
 				title: menuItemTitle,
 				link: menuItemURL,
 				menuItemType: menuItemType || 'item',
@@ -1276,7 +1278,7 @@ angular.module('app.core').service('Menus', [
 		};
 
 		// Add submenu item object
-		this.addSubMenuItem = function(menuId, rootMenuItemURL, menuItemTitle, menuItemURL, menuItemUIRoute, isPublic, roles, position) {
+		this.addSubMenuItem = function(menuId, rootMenuItemURL, menuItemTitle, menuItemURL, menuItemUIRoute, isPublic, roles, position, translateKey) {
 			// Validate that the menu exists
 			this.validateMenuExistance(menuId);
 
@@ -1291,7 +1293,8 @@ angular.module('app.core').service('Menus', [
 						isPublic: ((isPublic === null || typeof isPublic === 'undefined') ? this.menus[menuId].items[itemIndex].isPublic : isPublic),
 						roles: ((roles === null || typeof roles === 'undefined') ? this.menus[menuId].items[itemIndex].roles : roles),
 						position: position || 0,
-						shouldRender: shouldRender
+						shouldRender: shouldRender,
+						translate: translateKey
 					});
 				}
 			}
