915b2020026d1ec299bff4755ea0207de43ef232
isbiin <isalazar@biin.io>
2016-04-28 09:23:32 -0600

    removing nps

--- public/dist/application.js
+++ public/dist/application.js
@@ -1577,7 +1577,7 @@ angular.module('biins').config(['$stateProvider',
         Menus.addMenuItem('sidebar', 'Elements', 'elements', null, '/elements', false, null, null,'icon-book-open', "sidebar.MENU_ELEMENTS");
         Menus.addMenuItem('sidebar', 'Showcase', 'showcases', null, '/showcase', false, null, null,'icon-docs', "sidebar.MENU_SHOWCASES");
         Menus.addMenuItem('sidebar', 'Biins', 'biins', null, '/biins', false, null, null,'icon-feed', "sidebar.MENU_BIINS");
-        Menus.addMenuItem('sidebar', 'NPS', 'nps', null, null, false, null, null,'fa fa-bar-chart', "sidebar.MENU_NPS");
+        //Menus.addMenuItem('sidebar', 'NPS', 'nps', null, null, false, null, null,'fa fa-bar-chart', "sidebar.MENU_NPS");
 
                     //     menuId, menuItemTitle, menuItemURL, menuItemType, menuItemUIRoute, isPublic, roles, position,iconClass, translateKey, alert)
         Menus.addMenuItem('sidebar', 'Administration', 'profile', 'dropdown', null, false, null, null,'fa fa-gears', "sidebar.MENU_ADMINISTRATION");
--- public/modules/core/config/core.client.menu.js
+++ public/modules/core/config/core.client.menu.js
@@ -15,12 +15,12 @@
         Menus.addMenuItem('sidebar', 'Elements', 'elements', null, '/elements', false, null, null,'icon-book-open', "sidebar.MENU_ELEMENTS");
         Menus.addMenuItem('sidebar', 'Showcase', 'showcases', null, '/showcase', false, null, null,'icon-docs', "sidebar.MENU_SHOWCASES");
         Menus.addMenuItem('sidebar', 'Biins', 'biins', null, '/biins', false, null, null,'icon-feed', "sidebar.MENU_BIINS");
-        Menus.addMenuItem('sidebar', 'NPS', 'nps', null, null, false, null, null,'fa fa-bar-chart', "sidebar.MENU_NPS");
+        //Menus.addMenuItem('sidebar', 'NPS', 'nps', null, null, false, null, null,'fa fa-bar-chart', "sidebar.MENU_NPS");
 
-                    //     menuId, menuItemTitle, menuItemURL, menuItemType, menuItemUIRoute, isPublic, roles, position,iconClass, translateKey, alert)
+        //menuId, menuItemTitle, menuItemURL, menuItemType, menuItemUIRoute, isPublic, roles, position,iconClass, translateKey, alert)
         Menus.addMenuItem('sidebar', 'Administration', 'profile', 'dropdown', null, false, null, null,'fa fa-gears', "sidebar.MENU_ADMINISTRATION");
 
-                    //        menuId, rootMenuItemURL, menuItemTitle, menuItemURL, menuItemUIRoute, isPublic, roles, position, translateKey
+        //menuId, rootMenuItemURL, menuItemTitle, menuItemURL, menuItemUIRoute, isPublic, roles, position, translateKey
         Menus.addSubMenuItem('sidebar', 'profile', 'Profile','profile', '/profile', false, null, null, "sidebar.MENU_PROFILE");
         Menus.addSubMenuItem('sidebar', 'profile', 'Organizations','organization', '/organization', false, null, null, "sidebar.MENU_ORGANIZATIONS");
 
