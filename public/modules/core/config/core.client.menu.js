802bb0bc68fea64d145c034c7d2c85d5094f1b62
woxry <carloswd09@gmail.com>
2016-06-27 09:32:58 -0600

    Adding gifts to the sidebar

--- public/modules/core/config/core.client.menu.js
+++ public/modules/core/config/core.client.menu.js
@@ -15,6 +15,7 @@
         Menus.addMenuItem('sidebar', 'Showcase'     , 'showcases'       , null, '/showcase'     , false, null, null, 'icon-docs', "sidebar.MENU_SHOWCASES");
         Menus.addMenuItem('sidebar', 'Biins'        , 'biins'           , null, '/biins'        , false, null, null, 'icon-feed', "sidebar.MENU_BIINS");
         Menus.addMenuItem('sidebar', 'Sites'        , 'sites'           , null, '/sites'        , false, null, null, 'icon-pointer', "sidebar.MENU_SITES");
+        Menus.addMenuItem('sidebar', 'Gifts'        , 'gifts'           , null, '/gifts'        , false, null, null, 'icon-present', "sidebar.MENU_GIFTS");
         Menus.addMenuItem('sidebar', 'Organizations', 'organization'   , null, '/organization'  , false, null, null, 'icon-globe', "sidebar.MENU_ORGANIZATIONS");
         Menus.addMenuItem('sidebar', 'Profile'      , 'profile'         , null, '/profile'      , false, null, null, 'icon-user', "sidebar.MENU_PROFILE");
         //Maintenance has role field: maintenance
