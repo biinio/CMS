6fda15efe5e5adea65e68387a6ff926fcc8ff8ee
woxry <carloswd09@gmail.com>
2016-06-27 08:43:25 -0600

    Sidemenu Searchbox filter fix

--- public/dist/application.js
+++ public/dist/application.js
@@ -558,13 +558,14 @@ angular.module('biinUsers').config(['$stateProvider',
 
 (function() {
     'use strict';
-
+      
     angular
         .module('biinUsers')
         .controller('LoginFormController', LoginFormController);
 
-    LoginFormController.$inject = ['$http', '$state','$location','$scope','Authentication','Organization'];
-    function LoginFormController($http, $state,$location,$scope,Authentication,Organization) {
+    LoginFormController.$inject = ['$http', '$state','$location','$scope','$translate','Authentication','Organization'];
+
+    function LoginFormController($http, $state,$location,$scope,$translate,Authentication,Organization) {
         var vm = this;
         $scope.authentication = Authentication;
 
@@ -577,52 +578,46 @@ angular.module('biinUsers').config(['$stateProvider',
         ////////////////
 
         function activate() {
-          // bind here all data from the form
-          vm.account = {};
-          // place the message if something goes wrong
-          vm.authMsg = '';
-
-          vm.login = function() {
+            // Bind here all data from the form
+            vm.account = {};
+            // Place the message if something goes wrong
             vm.authMsg = '';
 
-            if(vm.loginForm.$valid) {
+            vm.login = function() {
+                vm.authMsg = '';
 
-              $http
-                .post('api/account/login', {email: vm.account.email, password: vm.account.password})
-                .then(function(response) {
-                  // assumes if ok, response is an object with some data, if not, a string with error
-                  // customize according to your api
-                  if ( !response.data.account ) {
-                    vm.authMsg = 'Incorrect credentials';
-                  }else{
-                      $scope.authentication.user = response.data.account;
-                      Organization.getSelectedOrganization().then(function() {
-                          Organization.getOrganizations().then( function() {
-                              $state.go('app.dashboard');
-                          });
-                      });
-
-                  }
-                }, function(reason) {
-                      console.log(reason);
-                      if (reason.status == "401") {
-                          vm.authMsg = 'Incorrect credentials';
-                      } else {
-                          vm.authMsg = 'Server Request Error';
-                      }
-                      $state.go('page.login');
-                });
-            }
-            else {
-              // set as dirty if the user click directly to login so we show the validation messages
-              /*jshint -W106*/
-              vm.loginForm.account_email.$dirty = true;
-              vm.loginForm.account_password.$dirty = true;
-            }
-          };
+                if(vm.loginForm.$valid) {
+                    $http
+                    .post('api/account/login', {email: vm.account.email, password: vm.account.password})
+                    .then(function(response) {
+                    // Assumes if ok, response is an object with some data, if not, a string with error
+                    // Customize according to your api
+                    if ( !response.data.account ) {
+                        vm.authMsg = $translate('LOGIN.INVALID_CREDENTIALS');
+                    }else{
+                        $scope.authentication.user = response.data.account;
+                        Organization.getSelectedOrganization().then(function() {
+                            Organization.getOrganizations().then( function() {
+                                $state.go('app.dashboard');
+                            });
+                        });
+                    }
+                    }, function(reason) {
+                        if (reason.status == "401") {
+                            vm.authMsg = $translate.instant('LOGIN.INVALID_CREDENTIALS');
+                        } else {
+                            vm.authMsg = $translate.instant('SERVER_ERROR');
+                        }
+                        $state.go('page.login');
+                    });
+                }else {
+                // Set as dirty if the user click directly to login so we show the validation messages
+                /*jshint -W106*/
+                vm.loginForm.account_email.$dirty = true;
+                vm.loginForm.account_password.$dirty = true;
+                }
+            };
         }
-
-
     }
 })();
 
