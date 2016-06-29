9b28aa72c92143cd94554a95bda0399fb71d000e
woxry <carloswd09@gmail.com>
2016-06-29 13:41:57 -0600

    Logo optimization - ui-sref in rounting -First CMS-UI updates

--- public/modules/core/config/core.client.menu.js
+++ public/modules/core/config/core.client.menu.js
@@ -10,16 +10,16 @@
         // Add default menu entry
         //Menus.addMenuItem('sidebar', 'Home', 'home', null, '/home', true, null, null, 'icon-home');
 
-        Menus.addMenuItem('sidebar', 'Dashboard'    , 'dashboard'       , null, '/dashboard'    , false, null, null, 'icon-speedometer', "sidebar.MENU_DASHBOARD");
-        Menus.addMenuItem('sidebar', 'Elements'     , 'elements'        , null, '/elements'     , false, null, null, 'icon-book-open', "sidebar.MENU_ELEMENTS");
-        Menus.addMenuItem('sidebar', 'Showcase'     , 'showcases'       , null, '/showcase'     , false, null, null, 'icon-docs', "sidebar.MENU_SHOWCASES");
-        Menus.addMenuItem('sidebar', 'Biins'        , 'biins'           , null, '/biins'        , false, null, null, 'icon-feed', "sidebar.MENU_BIINS");
-        Menus.addMenuItem('sidebar', 'Sites'        , 'sites'           , null, '/sites'        , false, null, null, 'icon-pointer', "sidebar.MENU_SITES");
-        Menus.addMenuItem('sidebar', 'Gifts'        , 'gifts'           , null, '/gifts'        , false, null, null, 'icon-present', "sidebar.MENU_GIFTS");
-        Menus.addMenuItem('sidebar', 'Organizations', 'organization'   , null, '/organization'  , false, null, null, 'icon-globe', "sidebar.MENU_ORGANIZATIONS");
-        Menus.addMenuItem('sidebar', 'Profile'      , 'profile'         , null, '/profile'      , false, null, null, 'icon-user', "sidebar.MENU_PROFILE");
+        Menus.addMenuItem('sidebar', 'Dashboard'    , 'dashboard'       , null, 'app.dashboard'    , false, null, null, 'icon-speedometer', "sidebar.MENU_DASHBOARD");
+        Menus.addMenuItem('sidebar', 'Elements'     , 'elements'        , null, 'app.elements'     , false, null, null, 'icon-book-open', "sidebar.MENU_ELEMENTS");
+        Menus.addMenuItem('sidebar', 'Showcase'     , 'showcases'       , null, 'app.showcases'     , false, null, null, 'icon-docs', "sidebar.MENU_SHOWCASES");
+        Menus.addMenuItem('sidebar', 'Biins'        , 'biins'           , null, 'app.biins'        , false, null, null, 'icon-feed', "sidebar.MENU_BIINS");
+        Menus.addMenuItem('sidebar', 'Sites'        , 'sites'           , null, 'app.sites'        , false, null, null, 'icon-pointer', "sidebar.MENU_SITES");
+        Menus.addMenuItem('sidebar', 'Gifts'        , 'gifts'           , null, 'app.gifts'        , false, null, null, 'icon-present', "sidebar.MENU_GIFTS");
+        Menus.addMenuItem('sidebar', 'Organizations', 'organization'   , null, 'app.organization'  , false, null, null, 'icon-globe', "sidebar.MENU_ORGANIZATIONS");
+        Menus.addMenuItem('sidebar', 'Profile'      , 'profile'         , null, 'app.profile'      , false, null, null, 'icon-user', "sidebar.MENU_PROFILE");
         //Maintenance has role field: maintenance
-        Menus.addMenuItem('sidebar', 'Maintenance', 'maintenance', null, '/maintenance', false, 'maintenance', null, 'icon-settings', "sidebar.MENU_MAINTENANCE");
+        Menus.addMenuItem('sidebar', 'Maintenance', 'maintenance', null, 'app.maintenance', false, 'maintenance', null, 'icon-settings', "sidebar.MENU_MAINTENANCE");
     }
 
 })();
