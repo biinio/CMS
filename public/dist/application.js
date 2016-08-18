e0ae26503bf2e1686dee8234111182feda8d7c8b
woxry <carloswd09@gmail.com>
2016-08-18 09:17:11 -0600

    Saving changes

--- public/dist/application.js
+++ public/dist/application.js
@@ -17,7 +17,7 @@ var ApplicationConfiguration = (function() {
 	var applicationModuleVendorDependencies = ['ngRoute', 'ngAnimate', 'ngStorage', 'ngTouch', 'ngCookies',
         'pascalprecht.translate', 'ui.bootstrap', 'ui.router', 'oc.lazyLoad', 'cfp.loadingBar', 'ngSanitize',
         'ngResource', 'ngMessages', 'ui.utils','ngAnimate', 'toaster','textAngular','bootstrap-tagsinput','angular-bind-html-compile',
-		'datePicker','ui.bootstrap-slider','ngDragDrop','dndLists','nvd3','ngImgCrop','color.picker','monospaced.qrcode'];
+		'datePicker','ui.bootstrap-slider','ngDragDrop','nvd3','ngImgCrop','color.picker','monospaced.qrcode', 'dndLists'];
 	// Add a new vertical module
 	var registerModule = function(moduleName, dependencies) {
 		// Create angular module
