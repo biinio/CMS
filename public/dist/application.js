af827e878e233a7b00fd9abeafbc93a925aef8aa
woxry <carloswd09@gmail.com>
2016-08-22 14:23:28 -0600

    Design changes

--- public/dist/application.js
+++ public/dist/application.js
@@ -2248,9 +2248,14 @@ angular.module('app.core').controller('HeaderController', ['$scope', 'Authentica
  */
 'use strict';
 
-angular.module('app.core').controller('LoadingController', ['$scope','Loading',
-    function($scope, LoadingService) {
+angular.module('app.core').controller('LoadingController', ['$rootScope','$scope','Loading', 'Utils',
+    function($rootScope, $scope, LoadingService, Utils) {
         $scope.loading = LoadingService;
+        $scope.isCollapse = Utils.isSidebarCollapsed();
+
+        $rootScope.$watch('app.layout.isCollapsed', function(newValue) {
+            $scope.isCollapse = newValue;
+        });
     }
 ]);
 
